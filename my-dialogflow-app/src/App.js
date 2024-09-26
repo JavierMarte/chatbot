f5a130db6c6750bc9316c82b88e05599ee1fcaf0
javiermartescaleg <mec112222@gmail.com>
2024-09-26 14:51:40 -0400

    adding final code

--- my-dialogflow-app/src/App.js
+++ my-dialogflow-app/src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './hyattlogo.png';
 import './App.css';
 
 function App() {
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        Welcome to Hyatt !
         </p>
         <a
           className="App-link"
@@ -15,7 +15,6 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
         </a>
       </header>
     </div>
@@ -23,3 +22,6 @@ function App() {
 }
 
 export default App;
+
+
+
